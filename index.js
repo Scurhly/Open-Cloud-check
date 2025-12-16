157fe8bd8093e67a7151f817e11d409f2156a768
Scurhly <rafaelong200208@gmail.com>
2025-12-16 09:52:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.post("/create-product", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
+
